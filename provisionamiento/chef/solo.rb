5bfbcdbe92dc5ba2ef837370fbb6a2daefa0df55
cesar2 <cesypozo@correo.ugr.es>
2016-11-24 17:44:17 +0100

    actualizando

---
+++ provisionamiento/chef/solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/home/ubuntu/Tripbot/provisionamiento/chef/cookbooks/tripbot/recipes"
+cookbook_path "/home/ubuntu/Tripbot/provisionamiento/chef/cookbooks"
+json_attribs "/home/ubuntu/Tripbot/provisionamiento/chef/node.json"
