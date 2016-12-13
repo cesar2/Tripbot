8a4c86b83191b252d934754331922cdf48b5114e
cesar2 <cesypozo@correo.ugr.es>
2016-12-13 17:57:00 +0100

    subiendo orquestacion

---
+++ orquestacion/solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/home/ubuntu/Tripbot/provisionamiento/chef"
+cookbook_path "/home/ubuntu/Tripbot/provisionamiento/chef/cookbooks"
+json_attribs "/home/ubuntu/Tripbot/provisionamiento/chef/node.json"
